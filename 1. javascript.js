5a7579d1a099ec6fb351d25d23afd7916e798e06
Sunaina Agarwal <56354115+Sunainacode@users.noreply.github.com>
2021-08-09 13:15:28 +0530

    Add files via upload

---
+++ 1. javascript.js
@@ -0,0 +1,33 @@
+// Introduction:
+// Javascript is a client side scripting language
+// It it used to make web pages alive
+// It it used to programmatically perform actions within the page
+// It was initially called LiveScript
+// But Java was a very popular language at that time so it was decided that positioning a language as a younger brother of Java would help.
+
+// What can JavaScript do?
+// JavaScript can execute not only in the browser but also on the server 
+// We use JavaScript as a client as well as server side language 
+
+// What can in-browser JavaScript do?
+// Safety
+// Does not give any low-level CPU permissions
+// Code runs in the browser
+// Add new HTML and change existing HTML from DOM(Document Object Model)
+// React to events(response from server, key press, mouse movement)
+// Ajax requests(Asynchronous JavaScript and XML)
+// Get and set cookies and use local storage
+
+// What can JavaScript not do?
+// Read / Write to and from computer hard-disk
+// Cannot take data from any parallel web page also called Same Origin Policy
+
+// What makes JavaScript unique?
+// Suppport of HTML/CSS
+// Simple things have simple APIs
+// Supported in major modern browsers. Enabled by default
+
+// JavaScript versions:
+// Maintained by community
+// ECMA(1997) is a scripting standard to maintain JavaScript. It provides new updates from time-to-time
+// ECMA updates every year
---
+++ 8. functions.js
@@ -0,0 +1,19 @@
+console.log("This is a tutorial")
+let name="Sunaina"
+let name1="Aayushi"
+let name2="Aruna"
+let name3="Archana"
+// console.log(name+ " is a good girl")
+// console.log(name1+ " is a good girl")
+// console.log(name2+ " is a good girl")
+// console.log(name3+ " is a good girl")
+
+function gg(name){
+    console.log(name+ " is a good girl")
+
+}
+
+gg(name);
+gg(name1);
+gg(name2);
+gg(name3);
